0b6d5fb8dfda9b17de695307b331cef3daf7d868
Szöky Markus <m.szoeky@COUNTIT.AT>
2025-07-03 16:08:56 +0200

    favicon

--- _site/test local.cmd
+++ _site/test local.cmd
@@ -1,4 +1,8 @@
+::gem install jekyll bundler
 cd C:\Work\digicamp
+::jekyll new --skip-bundle . --force
 ::bundle install
 bundle exec jekyll serve
 pause
+
+::https://docs.github.com/de/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
